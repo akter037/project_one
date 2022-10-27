f2b491381f44e1be55e7ea40d8f8f3cf718d2cb5
akter037 <94971719+akter037@users.noreply.github.com>
2022-10-28 05:28:10 +0600

    some section added

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -1,9 +1,69 @@
 import React from 'react'
 import './question.css'
+import {AiFillCaretDown} from 'react-icons/ai'
 
 const Question = () => {
   return (
-    <div>Question</div>
+  <section id='question' className='question_bg'>
+  <div className="container question_container">
+    <h2>Quection And  Answer</h2>
+    
+    <div className="questions">
+      <div className='question'>
+       <h3>why are you being a programmer?</h3>
+       <AiFillCaretDown size={26} className='icon'></AiFillCaretDown>
+       <div className='question_ans'>
+        <p>Because this section has a bright future. I am enjoying programming .</p>
+
+       </div>
+
+    </div>
+    <div className="question">
+       <h3>why are you being a programmer?</h3>
+       <AiFillCaretDown size={26} className='icon'></AiFillCaretDown>
+       <div className='question_ans'>
+        <p>Because this section has a bright future. I am enjoying programming .</p>
+
+       </div>
+       </div>
+       <div className="question">
+       <h3>why are you being a programmer?</h3>
+       <AiFillCaretDown size={26} className='icon'></AiFillCaretDown>
+       <div className='question_ans'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptatum dolorem dolor exercitationem adipisci beatae doloribus doloremque vel sint repudiandae, suscipit eveniet voluptates impedit in facere reiciendis, hic non magnam.</p>
+
+       </div>
+       </div>
+       <div className="question">
+       <h3>why are you being a programmer?</h3>
+       <AiFillCaretDown size={26} className='icon'></AiFillCaretDown>
+       <div className='question_ans'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptatum dolorem dolor exercitationem adipisci beatae doloribus doloremque vel sint repudiandae, suscipit eveniet voluptates impedit in facere reiciendis, hic non magnam.</p>
+
+       </div>
+       </div>
+       <div className="question">
+       <h3>why are you being a programmer?</h3>
+       <AiFillCaretDown size={26} className='icon'></AiFillCaretDown>
+       <div className='question_ans'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptatum dolorem dolor exercitationem adipisci beatae doloribus doloremque vel sint repudiandae, suscipit eveniet voluptates impedit in facere reiciendis, hic non magnam.</p>
+
+       </div>
+       </div>
+
+
+    </div>
+    
+
+
+
+  </div>
+
+
+
+
+  </section>
+    
   )
 }
 
--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -3,7 +3,43 @@ import './service.css'
 
 const Service = () => {
   return (
-    <div>Service</div>
+   <section id='service'>
+
+    <div className="container service_container">
+    
+    <h2>Step by step to the OKR introduction</h2>
+    <div className="services">
+   
+
+    <ul>
+  <li>
+    <h3>This section for Header</h3>
+    
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</li>
+  <li className='li_right'>
+  <h3>This section for Header</h3>
+    
+   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+  <li>
+  <h3>This section for Header</h3>
+  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
+  <li   className='li_right'>
+  <h3>This section for Header</h3>
+  Et harum quidem rerum facilis est et expedita distinctio.</li>
+
+  <li>
+  <h3>This section for Header</h3>
+  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
+
+   </ul>
+    </div>
+       
+
+
+
+    </div>
+
+   </section>
   )
 }
 
