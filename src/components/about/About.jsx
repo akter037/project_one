c2ff109291b7d0ee8f4b1b54fbaeab8d67554d44
akter037 <94971719+akter037@users.noreply.github.com>
2022-10-28 13:14:29 +0600

    About page Added

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/about_me.jpg'
 
 const About = () => {
   return (
-    <div>About</div>
+  <section id='about' className='about_bg'>
+      
+      <div className="container about_container">
+        <div className="about_me">
+             
+           <h2> Concentrate on the essentials</h2>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, error quisquam dolor sunt voluptatum repellat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem, totam</p>
+          <a  href="#contact"  className='btn btn-paimary'>Contact Us</a>
+        </div>
+        <div className="about_me_image">
+
+            <img src={ME} alt="me" />
+          
+        </div>
+
+
+
+
+
+
+      </div>
+
+
+  </section>
   )
 }
 
