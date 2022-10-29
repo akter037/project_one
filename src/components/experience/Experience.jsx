ce2e8627917d73b76fed4bfece0d245d40bca61f
akter037 <94971719+akter037@users.noreply.github.com>
2022-10-30 02:15:28 +0600

    services section added

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,7 +2,77 @@ import React from 'react'
 import './experience.css'
 const Experience = () => {
   return (
-    <div>Experience</div>
+  <section id='experience'>
+
+    <div className="container exerience_container">
+   
+       <h1> Our Services
+       </h1>
+       <div className="experience_contants">
+       <div className="experience_contant">
+          <h2>OKR Strategy Cell</h2>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+          
+          <div className='list_contant'>
+           <ul>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+           </ul>
+          </div>
+          <div className='button_experience'>
+
+<a href="#" className='btn btn-paimary'> learn More</a>
+</div>
+        </div>  
+
+        <div className="experience_contant">
+          <h2>OKR Strategy Cell</h2>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+           <div className='list_contant'>
+           <ul>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+            </ul>
+           </div>
+          
+   <div className='button_experience'>
+
+   <a href="#" className='btn btn-paimary'> learn More</a>
+   </div>
+            
+        
+        </div>  
+        <div className="experience_contant">
+          <h2>OKR Strategy Cell</h2>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+          
+          <div className='list_contant'>
+           <ul>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+            <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, voluptatem!</li>
+           </ul>
+          </div>
+          
+           <div className='button_experience'>
+
+   <a href="#" className='btn btn-paimary'> learn More</a>
+   </div>
+        
+        </div>    
+
+     
+
+    </div>
+    </div>
+
+  </section>
   )
 }
 
