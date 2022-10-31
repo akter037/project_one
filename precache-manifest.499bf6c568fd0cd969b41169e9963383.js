dbaa6723cb7a110c5fee22cfa450d1b10010135a
akter037 <94971719+akter037@users.noreply.github.com>
2022-11-01 02:10:17 +0600

    Updates

---
+++ precache-manifest.499bf6c568fd0cd969b41169e9963383.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed4591be57595f18143e",
+    "url": "/project_one/static/css/main.eac43788.chunk.css"
+  },
+  {
+    "revision": "ed4591be57595f18143e",
+    "url": "/project_one/static/js/main.ed4591be.chunk.js"
+  },
+  {
+    "revision": "e2e1d9e5ca996f33e8e3",
+    "url": "/project_one/static/js/1.e2e1d9e5.chunk.js"
+  },
+  {
+    "revision": "2e3d6a744e684097d519",
+    "url": "/project_one/static/js/runtime~main.2e3d6a74.js"
+  },
+  {
+    "revision": "3ca6e4b153de4194b554a551dbd959d9",
+    "url": "/project_one/static/media/my.3ca6e4b1.png"
+  },
+  {
+    "revision": "2f4dc3e10f8922274ec0db912e3d9139",
+    "url": "/project_one/static/media/about_me.2f4dc3e1.jpg"
+  },
+  {
+    "revision": "30452666221fdfff180bc6de851d1e06",
+    "url": "/project_one/static/media/IMG_1987.30452666.JPG"
+  },
+  {
+    "revision": "b99a2567c157aa0c0825f3cad1e1571d",
+    "url": "/project_one/static/media/header_bg.b99a2567.gif"
+  },
+  {
+    "revision": "eafa8b9c7a25dd03af5822b3f762f5af",
+    "url": "/project_one/index.html"
+  }
+];
